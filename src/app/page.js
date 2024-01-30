6c67f3857bd4cdb863eaf2607946cad1f87b1e30
ibharathboga <ibharathboga@gmail.com>
2024-01-30 18:45:18 +0530

    shadcn added, configured. button shown.

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,9 @@
+import { Button } from "@/components/shadcn/ui/button"
 export default function Home() {
     return (
         <>
             <h1 className="border border-red-500">hello from home page.</h1>
+            <Button>Click me</Button>
         </>
     );
 }
