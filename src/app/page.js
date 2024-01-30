d3caa877fa6f9e6e95303c201c6add2b030173cd
ibharathboga <ibharathboga@gmail.com>
2024-01-30 18:54:47 +0530

    shadcn button rescale added.

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,12 @@
+import Link from 'next/link';
 import { Button } from "@/components/shadcn/ui/button"
 export default function Home() {
     return (
         <>
             <h1 className="border border-red-500">hello from home page.</h1>
-            <Button>Click me</Button>
+            <Link href="/about" className='hover:underline underline-offset-4'>
+                <Button>Click me</Button>
+            </Link>
         </>
     );
 }
