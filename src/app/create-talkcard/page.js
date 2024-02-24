cd009a79cd116e82088339f583147c593af9f73b
ibharathboga <ibharathboga@gmail.com>
2024-02-24 11:51:49 +0530

    create-talkcard page, postTalkCard server action incorporated

--- src/app/create-talkcard/page.js
+++ src/app/create-talkcard/page.js
@@ -1,52 +1,95 @@
 "use client"
 import { useState } from "react";
 
+import { useForm } from "react-hook-form";
+import { postTalkCard } from "@/actions/postTalkCard";
+
 import { Input } from "@/components/shadcn/ui/input";
 import { Textarea } from "@/components/shadcn/ui/textarea";
 import { Button } from "@/components/shadcn/ui/button";
 
 export default function CreateTalkCard() {
-    const [talkCardTitle, setTalkCardTitle] = useState('')
-    const [talkCardDescription, setTalkCardDescription] = useState('')
 
     const maxTitleLength = 100
     const maxDescriptionLength = 300
 
-    const handleTalkCardTitleChange = (e) => {
-        if (e.target.value.length > maxTitleLength) { return }
-        setTalkCardTitle((prev) => e.target.value)
-    }
-    const handleTalkCardDescription = (e) => {
-        if (e.target.value.length > maxDescriptionLength) { return }
-        setTalkCardDescription((prev) => e.target.value)
+    const form = useForm()
+    const { register, handleSubmit, watch, formState } = form
+    const { errors } = formState
+
+    const handleOnSubmit = async (formData) => {
+        const xtalkcard = {
+            title: formData.title,
+            description: formData.description
+        }
+
+        // console.log(JSON.stringify(xtalkcard))
+
+        const response = await postTalkCard(xtalkcard)
+        console.log(response.message)
     }
 
     return (
-        <>
-            <div className="max-w-[500px] min-w-[300px] mx-auto p-3 space-y-5">
-                <div>
-                    <Input
-                        value={talkCardTitle}
-                        onChange={handleTalkCardTitleChange}
-                        placeholder="talkcard title"
-                    />
-                    <p className="text-sm text-muted-foreground text-right pr-2">
-                        {maxTitleLength - talkCardTitle.length}
+        <form
+            onSubmit={handleSubmit(handleOnSubmit)}
+            className="max-w-[500px] min-w-[300px] mx-auto p-3 space-y-5"
+        >
+            <div>
+                <Input
+                    {...register
+                        ("title",
+                            {
+                                required: {
+                                    value: true,
+                                    message: "title cannot be empty"
+                                },
+                                maxLength: {
+                                    value: maxTitleLength,
+                                    message: `max title length should not exceed ${maxTitleLength}`,
+                                }
+                            }
+                        )
+                    }
+                    placeholder="talkcard title"
+                />
+                <div className="flex justify-between">
+                    <p className="text-sm text-muted-foreground text-right mx-2">
+                        {errors.title?.message ? null : watch('title', "").length} / {maxTitleLength}
+                    </p>
+                    <p className="text-sm text-muted-foreground text-right mx-2">
+                        {errors.title?.message}
                     </p>
                 </div>
-                <div>
-                    <Textarea
-                        value={talkCardDescription}
-                        onChange={handleTalkCardDescription}
-                        className="min-h-[250px] resize-none"
-                        placeholder="talkcard description"
-                    />
-                    <p className="text-sm text-muted-foreground text-right pr-2">
-                        {maxDescriptionLength - talkCardDescription.length}
-                    </p>
-                </div>
-                <Button className="float-end" size="sm">Submit</Button>
             </div>
-        </>
+            <div>
+                <Textarea
+                    {...register
+                        ("description",
+                            {
+                                required: {
+                                    value: true,
+                                    message: "description cannot be empty"
+                                },
+                                maxLength: {
+                                    value: maxDescriptionLength,
+                                    message: `max description length should not exceed ${maxDescriptionLength}`,
+                                }
+                            }
+                        )
+                    }
+                    className="min-h-[250px] resize-none"
+                    placeholder="talkcard description"
+                />
+                <div className="flex justify-between">
+                    <p className="text-sm text-muted-foreground text-right pr-2 mx-2">
+                        {errors.description?.message ? null : watch('description', "").length}  / {maxDescriptionLength}
+                    </p>
+                    <p className="text-sm text-muted-foreground text-right pl-2 mx-2">
+                        {errors.description?.message}
+                    </p>
+                </div>
+            </div>
+            <Button className="float-end" size="sm">Submit</Button>
+        </form>
     );
 }
